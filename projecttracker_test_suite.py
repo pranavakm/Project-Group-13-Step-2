20249926f9d418e1bc516937f5db750bbaf41faf
Pranav Kumar Mahesh <pranava.km@gmail.com>
2023-12-09 19:35:49 -0800

    Changes made to test suite

--- projecttracker_test_suite.py
+++ projecttracker_test_suite.py
@@ -3,11 +3,14 @@ from TestFile_Handler import TestFile_Handler
 from TestInput_Handler import TestInput_Handler
 from TestOperations import TestOperations
 
-def projecttracker_test_suite():
+def my_suite():
     test_suite = unittest.TestSuite()
     result = unittest.TestResult()
     test_suite.addTest(unittest.makeSuite(TestFile_Handler))
     test_suite.addTest(unittest.makeSuite(TestInput_Handler))
     test_suite.addTest(unittest.makeSuite(TestOperations))
     runner = unittest.TextTestRunner()
+    print(runner.run(test_suite))
     return test_suite
+
+my_suite()
