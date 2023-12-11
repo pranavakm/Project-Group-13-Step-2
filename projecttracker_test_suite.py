44bfd3ae516b095d83d7da9e1f3923fed5211d36
Pranav Kumar Mahesh <pranava.km@gmail.com>
2023-12-10 18:39:56 -0800

    minor changes

--- projecttracker_test_suite.py
+++ projecttracker_test_suite.py
@@ -3,6 +3,8 @@ from TestFile_Handler import TestFile_Handler
 from TestInput_Handler import TestInput_Handler
 from TestOperations import TestOperations
 
+#The module, visualizations does not have a test class since this module only provides visualizations and does not return any values that can be used for testing.
+
 def my_suite():
     test_suite = unittest.TestSuite()
     result = unittest.TestResult()
